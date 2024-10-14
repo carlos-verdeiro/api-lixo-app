2599f42c445e7d5cc8f42676e0ea31dc62b6c922
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-10-13 22:40:12 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 print('Hell world!')
+=======
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
+>>>>>>> parent of 0a0664b (Update index.py)
