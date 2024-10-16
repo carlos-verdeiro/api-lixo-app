2437facbad4c3990a732495ac4a5ad1b163869e3
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-10-16 03:03:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-import os
-import base64
 from io import BytesIO
+import base64
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -25,12 +24,12 @@ def classificar_imagem(img_array):
     # Retorna a classe com a maior probabilidade
     return classes[np.argmax(prediction)]
 
-@app.route('/classificar', methods=['POST'])
+@app.route('/classificar', methods=['POST'])  # Alterado para POST
 def classificar():
     data = request.get_json()
 
     # Verifica se a requisição contém a imagem
-    if 'inputs' not in data or len(data['inputs']) == 0 or 'data' not in data['inputs'][0]:
+    if not data or 'inputs' not in data or len(data['inputs']) == 0 or 'data' not in data['inputs'][0]:
         return jsonify({'error': 'Nenhuma imagem enviada.'}), 400
     
     base64_image = data['inputs'][0]['data']['image']['base64']
@@ -38,22 +37,23 @@ def classificar():
     # Decodifica a imagem base64
     try:
         img_data = base64.b64decode(base64_image)
-    except Exception as e:
+    except Exception:
         return jsonify({'error': 'Falha ao decodificar a imagem.'}), 400
 
     # Carrega a imagem em um formato utilizável
     try:
         img = image.load_img(BytesIO(img_data), target_size=(128, 128))
-    except Exception as e:
+    except Exception:
         return jsonify({'error': 'Erro ao carregar a imagem.'}), 400
 
+    # Converte a imagem em array e normaliza
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)  # Adiciona a dimensão do batch
     
-    # Classificar a imagem
+    # Classifica a imagem
     resultado = classificar_imagem(img_array)
 
     return jsonify({'classe': resultado})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Permite acesso externo
